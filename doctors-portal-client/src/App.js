11a3a438e4134de4976f2a4dcf07e9f43ec87f92
rafidmahin20 <submanmahin@gmail.com>
2022-05-16 21:40:49 +0600

    first commit

---
+++ doctors-portal-client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 className='text-3xl'>Hello there</h2>
+    </div>
+  );
+}
+
+export default App;
