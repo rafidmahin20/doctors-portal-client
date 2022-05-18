ae9ac6648e552d1946da3a7450fbd2102c986bd1
rafidmahin20 <submanmahin@gmail.com>
2022-05-18 13:45:15 +0600

    edited

--- doctors-portal-client/src/Pages/Home/Home.js
+++ doctors-portal-client/src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h1>hello there</h1>
+        <div className='px-12'>
+           <Banner></Banner>
         </div>
     );
 };
