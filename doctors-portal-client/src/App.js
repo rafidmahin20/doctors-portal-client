54ade644cebb50870ebd8c4da86bca5fc4300200
rafidmahin20 <submanmahin@gmail.com>
2022-05-18 13:35:51 +0600

    edited

--- doctors-portal-client/src/App.js
+++ doctors-portal-client/src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
      <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+     </Routes>
     </div>
   );
 }
--- doctors-portal-client/src/Pages/Header/Header.js
+++ doctors-portal-client/src/Pages/Header/Header.js
@@ -4,17 +4,16 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="shadow-sm">
-        <div className="max-w-screen-xl p-4 mx-auto">
+        <div className= 'max-w-screen-xl p-4 mx-auto'>
           <div className="flex items-center justify-between space-x-4 lg:space-x-10">
             <div className="flex lg:w-0 lg:flex-1">
               <h3 className=' text-3xl font-bold'>Doctors Portal</h3>
             </div>
       
             <nav className="hidden space-x-8 text-sm font-medium md:flex">
-              <Link to='/' className="text-gray-500" href="">Home</Link>
-              <Link to='/blog' className="text-gray-500" href="">Blog</Link>
-              <Link to='/projects' className="text-gray-500" href="">Projects</Link>
-              <Link to='/contact' className="text-gray-500" href="">Contact</Link>
+            <Link to='/' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">Home</Link>
+                    <Link to='/about' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">About</Link>
+                    <Link to='/contact' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">Contact</Link>
             </nav>
       
             <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
