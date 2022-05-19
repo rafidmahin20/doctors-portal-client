bef08f60d5b5e9907de484f5fd1e0da97fca56dd
rafidmahin20 <submanmahin@gmail.com>
2022-05-19 22:21:44 +0600

    edited

--- doctors-portal-client/src/Pages/Header/Header.js
+++ doctors-portal-client/src/Pages/Header/Header.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header className="shadow-sm">
-        <div className= 'max-w-screen-xl p-4 mx-auto'>
-          <div className="flex items-center justify-between space-x-4 lg:space-x-10">
-            <div className="flex lg:w-0 lg:flex-1">
-              <h3 className=' text-3xl font-bold'>Doctors Portal</h3>
+  return (
+    
+    <nav className="bg-white shadow dark:bg-gray-800">
+        <div className="container px-6 py-4 mx-auto">
+            <div className="flex items-center justify-between md:flex md:items-center md:justify-between">
+                <div className="flex items-center justify-between">
+                    <div className="text-xl font-semibold text-gray-700">
+                        <h1 className="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Doctors Portal</h1>
+                    </div>
+
+                
+                    <div className="flex  md:hidden">
+                        <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                            <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+                                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div className="flex-1  md:flex md:items-center md:justify-between">
+                    <div className="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+                        <Link to='/' className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Home</Link>
+                        <Link to='/appointment' className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Appointment</Link>
+                        <Link to='/abput' className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">About</Link>
+                    </div>
+
+                    <div className="flex items-center mt-4 md:mt-0">
+                        <button className="hidden mx-4 text-gray-600 transition-colors duration-200 transform md:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none" aria-label="show notifications">
+                            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+
+                       
+                    </div>
+                </div>
             </div>
-      
-            <nav className="hidden space-x-8 text-sm font-medium md:flex">
-            <Link to='/' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">Home</Link>
-                    <Link to='/about' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">About</Link>
-                    <Link to='/appointment' className="px-2 py-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded dark:text-black hover:bg-gray-900 hover:text-gray-100 md:mx-2">Appointment</Link>
-            </nav>
-      
-            <div className="items-center justify-end flex-1 hidden space-x-4 sm:flex">
-                <button className="btn btn-primary px-5 py-2 text-sm font-medium text-white bg-red-400 rounded-lg">
-                    Login
-                </button>
-            </div>
-          </div>
         </div>
-      </header>
-    );
+    </nav>
+  );
 };
 
 export default Header;
--- doctors-portal-client/src/Pages/Home/Banner.js
+++ doctors-portal-client/src/Pages/Home/Banner.js
@@ -7,9 +7,9 @@ const Banner = () => {
             <div className="items-center lg:flex">
                 <div className="w-full lg:w-1/2">
                     <div className="lg:max-w-lg">
-                        <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-black lg:text-3xl">Best Place To Choose Your Clothes</h1>
+                        <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-black lg:text-3xl">Your New Smile Starts Here</h1>
                         <p className="mt-2 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
-                        <button className="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                        <button className="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Get Started</button>
                     </div>
                 </div>
         
